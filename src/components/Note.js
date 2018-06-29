0088fc15d9347edb5bcf3405ae2ec93eb80345b8
JB4GDI <JB4GDI@gmail.com>
2018-06-29 16:45:12 -0400

    No longer able to add notes to an uncreated note

--- src/components/Note.js
+++ src/components/Note.js
@@ -11,33 +11,44 @@ class Note extends React.Component {
     this.props.submitNote(formData, this.props.note.id);
   }
 
-  renderTagForm() {
+  onTagSubmit(e) {
+    e.preventDefault();
+    const formData = {
+      name: this.name.value
+    };
+    this.props.submitTag(formData, this.props.note.id)
+    this.props.closeTagForm();
+  }
 
-    if (!this.props.newTag) {
-      return (
-        <span>
-          Tag your note:
-          <i 
-            className="tag-button material-icons"
-            onClick={() => this.props.showTagForm() }
-          >
-            add circle
-          </i>
-        </span>
-      );  
-    } else {
-      return(
-        <form>
-          <input 
-            className="tag-input"
-            type="text"
-            placeholder="Tag name..."
-          />
-        </form>
-      );
+  renderTagForm(note) {
 
+    if (note.id !== undefined) {
+      if (!this.props.newTag) {
+        return (
+          <span>
+            Tag your note:
+            <i 
+              className="tag-button material-icons"
+              onClick={() => this.props.showTagForm() }
+            >
+              add circle
+            </i>
+          </span>
+        );  
+      } else {
+        return(
+          <form onSubmit={(e) => this.onTagSubmit(e)}>
+            <input 
+              className="tag-input"
+              type="text"
+              placeholder="Tag name..."
+              ref={(input) => this.name = input}
+            />
+          </form>
+        );
+
+      }
     }
-
   }
 
   render() {
@@ -70,7 +81,7 @@ class Note extends React.Component {
         <div className="tag-container">
 
           <div className="tag-button-container">
-            {this.renderTagForm()}
+            {this.renderTagForm(note)}
           </div>
 
         </div>
